da75dea596e81cf84bbeab1c3070c49e36596958
basmaskaik-beep <basma.jalal.skaik@gmail.com>
2026-01-29 21:08:27 +0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -3,10 +3,26 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+// Root API
 app.get('/', (req, res) => {
-  res.send('Gaza News Backend is running');
+  res.json({
+    status: 'ok',
+    message: 'Gaza News Backend is running'
+  });
+});
+
+// News API
+app.get('/news', (req, res) => {
+  res.json([
+    {
+      id: 1,
+      title: 'Gaza update',
+      source: 'Example source',
+      date: '2026-01-29'
+    }
+  ]);
 });
 
 app.listen(PORT, () => {
-  console.log('Server running on port ${PORT}');
+  console.log(Server running on port ${PORT});
 });
