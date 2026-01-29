650062cd2b296e28170066ffb39eff2c4eae3519
basmaskaik-beep <basma.jalal.skaik@gmail.com>
2026-01-29 22:31:58 +0300

    Update scraper.js

--- src/scraper.js
+++ src/scraper.js
@@ -1,21 +1,28 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
 
-const URL = 'https://www.motqdmon.com/search/label/%D8%A7%D9%84%D9%85%D8%B3%D8%A7%D8%B9%D8%AF%D8%A7%D8%AA';
+const URL =
+  'https://www.motqdmon.com/search/label/%D8%A7%D9%84%D9%85%D8%B3%D8%A7%D8%B9%D8%AF%D8%A7%D8%AA';
 
 async function scrapeNews() {
   try {
-    const { data } = await axios.get(URL);
-    const $ = cheerio.load(data);
+    const { data } = await axios.get(URL, {
+      headers: {
+        ’User-Agent’:
+          'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36'
+      }
+    });
 
+    const $ = cheerio.load(data);
     const news = [];
 
-    $('.post-title').each((i, element) => {
-      if (i < 10) {
+    $('.post-title').each((index, element) => {
+      if (index < 10) {
         const title = $(element).text().trim();
         const link = $(element).find('a').attr('href');
 
         news.push({
+          id: index + 1,
           title,
           link
         });
