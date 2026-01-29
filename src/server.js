c3735b61914e1baa1436aaccef81437d218f1350
basmaskaik-beep <basma.jalal.skaik@gmail.com>
2026-01-29 22:40:02 +0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const scrapeNews = require('./scraper');
 const app = express();
+const likes = {};
 const PORT = process.env.PORT || 3000;
 // Root API
 app.get('/', (req, res) => {
@@ -13,13 +14,32 @@ app.get('/', (req, res) => {
 app.get('/news', async (req, res) => {
   const news = await scrapeNews();
 
-  res.json({
-    status: 'ok',
-    count: news.length,
-    data: news
-  });
+const newsWithLikes = news.map(item => ({
+  ...item,
+  likes: likes[item.id] || 0
+}));
+
+res.json({
+  status: 'ok',
+  count: newsWithLikes.length,
+  data: newsWithLikes
 });
 
+  app.post('/news/:id/like', (req, res) => {
+  const id = req.params.id;
+
+  if (!likes[id]) {
+    likes[id] = 0;
+  }
+
+  likes[id]++;
+
+  res.json({
+    status: 'ok',
+    message: 'Like added',
+    likes: likes[id]
+  });
+});
 app.listen(PORT, () => {
   console.log(Server running on port ${PORT});
 });
